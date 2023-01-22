ab094189416abebc57551e1c6f626ebb4f9e4087
Luca Ilari <54724171+Trenord@users.noreply.github.com>
2023-01-22 22:35:05 +0100

    Update db.sql

---
+++ db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `locali`
+(
+ `id_locale`            int NOT NULL ,
+ `nome_locale`   varchar(45) NOT NULL ,
+ `long`          float NOT NULL ,
+ `lat`           float NOT NULL ,
+ `prezzo_spritz` float NOT NULL ,
+ `descrizione`   text NULL ,
+ `ip_creazione`  varchar(15) NOT NULL ,
+ `data`          datetime NOT NULL ,
+
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `recensioni`
+(
+ `id_recensione` int NOT NULL ,
+ `id_locale`     int NOT NULL ,
+ `utente`        text NOT NULL ,
+ `testo`         text NOT NULL ,
+
+PRIMARY KEY (`id_recensione`),
+KEY `FK_2` (`id_locale`),
+CONSTRAINT `FK_1` FOREIGN KEY `FK_2` (`id_locale`) REFERENCES `locali` (`id_locale`)
+);
+
